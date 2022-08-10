f655b85c4989e1182e01985509490bcb84d17e74
Harsh Shah <harshshah1295@gmail.com>
2022-08-10 12:59:34 -0300

    added daily challenge

---
+++ daily-challenge/108-convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,33 @@
+from typing import List, Optional
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+
+        def build_tree(l, r, nums):
+            if l > r:
+                return None
+
+            if l == r:
+                return TreeNode(nums[l])
+            
+            m = (l+r) // 2
+            left = build_tree(l, m-1, nums)
+            right = build_tree(m+1, r, nums)
+
+            return TreeNode(nums[m], left, right)
+
+        ln = len(nums)
+        if ln == 1:
+            return TreeNode(nums[0])
+
+        l, r = 0, len(nums) - 1
+        return build_tree(l, r, nums)
