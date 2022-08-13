e1ed0b9e7d6124521cdd93920f65b426509d131a
Harsh Shah <harshshah1295@gmail.com>
2022-08-13 20:43:45 -0300

    75-study-plan level-2 day-2

---
+++ 75-study-plan/level-2/multiply-strings.py
@@ -0,0 +1,37 @@
+from pprint import pprint
+
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # table = [[i*j for i in range(10)] for j in range(10)]
+
+        if num1 == "0" or num2 == "0":
+            return "0"
+        elif num1 == "1":
+            return num2
+        elif num2 == "1":
+            return num1
+        
+
+        l1, l2 = len(num1), len(num2)
+
+        num1, num2 = num1[::-1], num2[::-1]
+        result = [0] * (l1 + l2)
+        
+        for j, n2 in enumerate(num2):
+            for i, n1 in enumerate(num1):
+                x, y = int(n1), int(n2)
+
+                mul = (x * y) + result[i+j]
+                buffer = mul % 10
+                carry = mul // 10
+
+                result[i+j] = buffer 
+                result[i+j+1] += carry
+
+        while result[-1] == 0:
+            result.pop()
+
+        return "".join(map(str, result))
+
+print(Solution().multiply("123", "456"))
