a7f38c6927f14390d02e834a1d5523f4f8d0c286
Harsh Shah <harshshah1295@gmail.com>
2022-08-14 12:45:17 -0300

    google question practice

--- google/text-justification.py
+++ google/text-justification.py
@@ -5,46 +5,40 @@ from typing import List
 class Solution:
     def fullJustify(self, words: List[str], max_width: int) -> List[str]:
 
+        spacing_repo = {}
+
         def justify_line(words, words_width, space_required, max_width):
             no_words = len(words)
+            remaining_width = max_width - words_width
+            space_between = None
 
-            remaining_width = max_width - (words_width + space_required)
-
-            no_space_between_words = 1
-            space_between = [no_space_between_words] * space_required
-
-            if remaining_width > 0:
-
-                if space_required != 0:
-                    no_space_between_words += (remaining_width //
-                                               space_required)
-                    total_spacing = no_space_between_words * space_required
-                    space_between = [no_space_between_words] * space_required
-                    remaining_width = max_width - (words_width + total_spacing)
-                    for i in range(remaining_width):
-                        space_between[i] += 1
-                else:
-                    space_between = [max_width - words_width]
+            if space_required != 0:
+                no_space_between_words = (remaining_width // space_required)
+                total_spacing = no_space_between_words * space_required
+                space_between = [no_space_between_words] * space_required
+                remaining_width = max_width - (words_width + total_spacing)
+                for i in range(remaining_width):
+                    space_between[i] += 1
+            else:
+                space_between = [max_width - words_width]
 
             for idx, space in zip(range(1, no_words, 2), space_between):
-                words[idx] = " " * space
-
+                if space not in spacing_repo:
+                    spacing_repo[space] = " " * space
+                words[idx] = spacing_repo[space]
             return "".join(words)
 
         result = []
 
-        word_width = 0
-        spacing = -1
-        line = []
+        word_width, spacing, line = 0, -1, []
+
         for word in words:
             wl = len(word)
 
             if (word_width + wl + spacing + 1) > max_width:
                 result.append(justify_line(
                     line, word_width, spacing, max_width))
-                line = []
-                spacing = -1
-                word_width = 0
+                word_width, spacing, line = 0, -1, []
 
             word_width += wl
             spacing += 1
@@ -52,22 +46,13 @@ class Solution:
             line.append(word)
             line.append("")
 
-        if line:
-            line.pop()
 
-            for idx in range(1, len(line), 2):
-                line[idx] = " "
+        line.pop()
+        for idx in range(1, len(line), 2):
+            line[idx] = " "
 
-            last_line = "".join(line)
-            remaining_width = max_width - len(last_line)
-            result.append(last_line + " " * remaining_width)
+        last_line = "".join(line)
+        remaining_width = max_width - len(last_line)
+        result.append(last_line + " " * remaining_width)
 
         return result
-
-
-pprint(Solution().fullJustify(
-    ["This", "is", "an", "example", "of", "text", "justification."], 16))
-pprint(Solution().fullJustify(
-    ["What", "must", "be", "acknowledgment", "shall", "be"], 16))
-pprint(Solution().fullJustify(
-    ["Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"], 20))
