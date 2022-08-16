8ca037d4a94a227116489882d1035b3d2aeb92ab
Harsh Shah <harshshah1295@gmail.com>
2022-08-16 19:46:08 -0300

    google question practice

---
+++ google/arrays-and-strings/first-unique-character-in-a-string.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        unique_chars = defaultdict(lambda: 0)
+        visited = [False] * 26
+        unicode_a = ord("a")
+        for idx, sch in enumerate(s):
+            pos = ord(sch) - unicode_a
+            
+            if visited[pos]:
+                if sch in unique_chars:
+                    unique_chars.pop(sch)
+                continue
+            
+            unique_chars[sch] = idx
+            visited[pos] = True
+
+        return min(unique_chars.values()) if unique_chars else -1
+
+print(Solution().firstUniqChar("leetcode"))
+        
