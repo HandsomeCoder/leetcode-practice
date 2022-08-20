bec8aa5a34eda6a95abdbdd35d346a7710fdebbe
Harsh Shah <harshshah1295@gmail.com>
2022-08-20 14:58:02 -0300

    google question practice

---
+++ google/arrays-and-strings/857-minimum-cost-to-hire-k-workers.py
@@ -0,0 +1,46 @@
+from collections import deque
+from fractions import Fraction
+from heapq import heapify, heappop, heappush
+from math import inf
+from typing import List
+
+
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+
+        if k == 1:
+            return round(min(wage), 5)
+
+        ln = len(quality)
+        workers = [(Fraction(wage[i], quality[i]), quality[i])
+                   for i in range(ln)]
+
+        if k == ln:
+            return float(max(workers, key=lambda x: x[0])[0]) * sum(list(map(lambda x: x[1], workers)))
+
+        workers.sort()
+        min_wages = inf
+
+        pool = []
+        heapify(pool)
+        total_quality = 0
+        for ratio, q in workers:
+    
+            heappush(pool, -q)
+            total_quality += q
+            if len(pool) > k:
+                total_quality += heappop(pool)
+
+            if len(pool) == k:
+                wages = ratio * total_quality
+                min_wages = min(min_wages, wages)
+
+        return round(float(min_wages), 5)
+
+
+print(Solution().mincostToHireWorkers(
+   
+[5,2,3],
+[8,20,20],
+3
+))
