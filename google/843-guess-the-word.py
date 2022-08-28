a98eb2335b57b9ec39d3c776ad0c7c45decc10c5
Harsh Shah <harshshah1295@gmail.com>
2022-08-28 17:54:24 -0300

    google question practice

--- google/843-guess-the-word.py
+++ google/843-guess-the-word.py
@@ -1,57 +1,64 @@
+from typing import List
+
+
 # """
 # This is Master's API interface.
 # You should not implement it, or speculate about its implementation
 # """
-from collections import Counter, defaultdict
-from typing import List
-
-
-class Master:
-    def guess(self, word: str) -> int:
-        return 1
+# class Master:
+#     def guess(self, word: str) -> int:
 
 
 class Solution:
     def findSecretWord(self, words: List[str], master: 'Master') -> None:
 
+        def get_similarity(word1, word2):
+            count = 1
+            for idx in range(6):
+                if word1[idx] == word2[idx]:
+                    count += 1
+            return count
+
+        wln = len(words)
+        for i in range(wln):
+            similarity = 0
+            for j in range(i+1, wln):
+                similarity = max(similarity, get_similarity(words[i], words[j]))
+            
+            words[i] = (similarity, words[i])
 
         words.sort()
-        match_count, idx, aln = 0, 0, len(words)
+        
+        match_count = 0
         blacklisted_letters = [set() for _ in range(6)]
+        whitelisted_word = None
 
-        for idx in range(aln):
-            is_p_word = True
-            word = words[idx]
+        for idx in range(wln):
+            is_p_word = False
+            word = words[idx][1]
+            
             for idx, wch in enumerate(word):
                 if wch in blacklisted_letters[idx]:
                     is_p_word = False
                     break
 
+                if whitelisted_word == None:
+                    is_p_word = True
+                    continue
+                
+                if wch == whitelisted_word[idx]:
+                    is_p_word = True
+            
             if not is_p_word:
                 continue
-
-            if match_count > 0:
-                is_p_word = False
-                for wlch, wch in zip(whitelisted_word, word):
-                    if wch == wlch:
-                        is_p_word = True
-                        break
-
-            if not is_p_word:
-                continue
-
 
             count = master.guess(word)
-            
+
             if count == 0:
                 for idx, wch in enumerate(word):
                     blacklisted_letters[idx].add(wch)
             elif count > match_count:
                 match_count = count
+                whitelisted_word = word
                 if match_count == 6:
                     break
-
-                whitelisted_word = word
-
-print(Solution().findSecretWord(
-    ["acckzz", "ccbazz", "eiowzz", "abcczz"], Master()))
