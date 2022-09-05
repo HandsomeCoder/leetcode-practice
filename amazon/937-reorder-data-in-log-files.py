ac9af4e30a0fe310efa9683698fa4aac1a1d422f
Harsh Shah <harshshah1295@gmail.com>
2022-09-05 11:21:08 -0300

    amazon question practice

--- amazon/937-reorder-data-in-log-files.py
+++ amazon/937-reorder-data-in-log-files.py
@@ -5,35 +5,9 @@ from typing import List
 
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        def parse(log):
-            ln = len(log)
-            id, first_value = [], []
-            split_idx = -1
-            for idx in range(ln):
-                lch = log[idx]
-                if lch == " ":
-                    split_idx = idx
-                    break
-                id.append(lch)
+        def compare(log):
+            _id, body = log.split(maxsplit=1)
+            return (1, ) if body[0].isdigit() else (0, body, _id)
 
-            for idx in range(split_idx + 1,ln):
-                lch  = log[idx]
-                if lch == " ":
-                    break
-                first_value.append(lch)
-
-
-            return ("".join(id), "".join(first_value), log[split_idx+1:])
-
-        group = defaultdict(list)
-
-        for log in logs:
-            id, first_value, body = parse(log)
-            if first_value.isdigit():
-                group["digit"].append(log)
-            else:
-                group["letter"].append(body, id)
-
-        group["letter"].sort()
-
-        return [id + " " + body for body, id in group["letter"]] + group["digit"]
+        logs.sort(key=compare)
+        return logs
