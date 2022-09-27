18e396acac33d0edf93036fe3485271437441995
Harsh Shah <harshshah1295@gmail.com>
2022-09-27 13:17:48 -0300

    google question practice

--- google/design/146-lru-cache.py
+++ google/design/146-lru-cache.py
@@ -1,74 +1,73 @@
 class Node:
-    def __init__(self, val) -> None:
-        self.next = None
-        self.prev = None
-        self.val = val
-
+    def __init__(self, data) -> None:
+        self.prev = self.next = None
+        self.data = data
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.head = self.tail = Node(None)
-        self.limit = capacity
+        self.head, self.tail = Node(None), Node(None)
+        self.capacity = capacity
         self.length = 0
         self.mapping = {}
 
-    def increase_priority(self, curr):
-        if curr == self.tail:
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+
+    def __insert_left(self, node):
+        node.next = self.head.next
+        node.next.prev = node
+        self.head.next = node
+        node.prev = self.head
+
+    def __remove(self, node):
+        node.next.prev = node.prev
+        node.prev.next = node.next
+        node.next = node.prev = None
+
+    def __pop_right(self):
+        node = self.tail.prev
+        self.__remove(node)
+        return node
+
+
+    def __move_to_head(self, node):
+        if self.head.next == node:
             return
 
-        self.tail.next = curr.next
-        curr.next.prev = self.tail
+        self.__remove(node)
+        self.__insert_left(node)
 
-        curr.next = self.tail.prev
-        curr.prev.next = self.tail
-        self.tail.prev = curr.prev 
-        
-        curr.prev = curr.next
-        curr.prev.next = curr
-
-
+    def __clear_node(self, node):
+        node.data = node.next = node.prev = None
+        node = None
 
     def get(self, key: int) -> int:
         if key not in self.mapping:
             return -1
 
-        self.increase_priority(self.mapping[key])
-        return self.mapping[key].val[1]
+        self.__move_to_head(self.mapping[key])
+        return self.mapping[key].data[1]
 
     def put(self, key: int, value: int) -> None:
         
+        data = (key, value)
+
         if key in self.mapping:
             node = self.mapping[key]
-            node.val = (key, value)
-            self.increase_priority(node)
+            node.data = data
+            self.__move_to_head(node)
 
-        else:        
-            if self.length == self.limit:
-                top = self.head.next
-                self.mapping.pop(top.val[0])
-                self.head.next = top.next
-                top.next.prev = self.head
-
-                top.val = top.prev = top.next = None
-                top = None
-
-                self.length -= 1
-
-            self.mapping[key] = node = Node((key, value))
-            self.tail.next = node
-            node.prev = self.tail
-            self.tail = self.tail.next
+        else: 
+            node = Node(data)
+            self.mapping[key] = node
+            self.__insert_left(node)
             self.length += 1
 
 
-lRUCache = LRUCache(2)
-lRUCache.put(1, 1)
-lRUCache.put(2, 2)
-lRUCache.get(1)
-lRUCache.put(3, 3)
-lRUCache.get(2)
-lRUCache.put(4, 4)
-lRUCache.get(1)
-lRUCache.get(3)
-lRUCache.get(4)
+            if self.length > self.capacity:
+                node = self.__pop_right()
+                self.mapping.pop(node.data[0])
+                self.__clear_node(node)
+                self.length -= 1
