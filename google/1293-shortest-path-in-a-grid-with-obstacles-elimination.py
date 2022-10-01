9a2dea2988d1c1f8049b8e97458d5dfaa40ec141
Harsh Shah <harshshah1295@gmail.com>
2022-10-01 17:23:14 -0300

    google question practice

--- google/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ google/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,48 +1,69 @@
 from collections import deque
-from pprint import pprint
 from typing import List
 
 
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         R, C = len(grid), len(grid[0])
-        max_value = 40 * 40
-        s_matrix = [[max_value for _ in range(C)] for _ in range(R)]
-        not_visited = [[True for _ in range(C)] for _ in range(R)]
 
-        not_visited[0][0] = False
-        s_matrix[0][0] = 0
+        if k >= R + C - 2:
+            return R + C - 2
 
-        queue = deque([(0, 0, 0, 0)])
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        max_value = 2000
+        s_matrix = [[None for _ in range(C)] for _ in range(R)]
+        s_matrix[-1][-1] = (0, 0)
+        max_reach = (max_value, k)
 
+        def find_min_path(x, y, obj):
+            if s_matrix[x][y]:
+                return s_matrix[x][y]
+
+            if grid[x][y] == 1:
+                obj += 1
+
+            directions = [(x, y+1), (x+1, y)]
+            e_move, e_obj = max_reach
+            for nx, ny in directions:
+                if nx < R and ny < C:
+                    min_moves, min_obj = find_min_path(nx, ny, 0)
+                    total_obj = obj + min_obj
+                    if total_obj > k:
+                        continue
+
+                    total_moves = min_moves + 1
+                    if e_move == total_moves:
+                        e_obj = min(e_obj, total_obj)
+                    elif e_move > total_moves:
+                        e_move, e_obj = total_moves, total_obj
+
+            s_matrix[x][y] = (e_move, e_obj)
+            return s_matrix[x][y]
+
+        for r in reversed(range(R)):
+            for c in reversed(range(C)):
+                if grid[r][c] == 1:
+                    find_min_path(r, c, 0)
+
+        queue = deque([(0, 0, 0)])
+        min_moves = max_value
         while queue:
-            x, y, move, ob = queue.popleft()
-            not_visited[x][y] = False
+            x, y, move = queue.popleft()
+            if grid[x][y] == "#":
+                continue
 
-            for dx, dy in directions:
-                nx = x + dx
-                ny = y + dy
+            grid[x][y] = "#"
+            s_matrix[x][y] = None if s_matrix[x][y] == max_reach else s_matrix[x][y]
+            
+            if s_matrix[x][y]:
+                min_moves = min(min_moves, move + s_matrix[x][y][0])
+                continue
 
-                if 0 <= nx < R and 0 <= ny < C and not_visited[nx][ny]:
-                    s_matrix[nx][ny] = min(s_matrix[nx][ny], move + 1)
+            for nx, ny in [(x, y+1), (x+1, y), (x, y-1), (x-1, y)]:
+                if 0 <= nx < R and 0 <= ny < C and grid[nx][ny] != "#":
                     if grid[nx][ny] == 0:
-                        queue.append((nx, ny, move + 1, ob))
-                    elif ob < k:
-                        queue.append((nx, ny, move + 1, ob + 1))
+                        queue.append((nx, ny, move + 1))
+                    else:
+                        min_moves = min(min_moves, move +
+                                        1 + s_matrix[nx][ny][0])
 
-        return -1 if s_matrix[-1][-1] == max_value else s_matrix[-1][-1]
-
-pprint(Solution().shortestPath(
-    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], 
-    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1], 
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], 
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0], 
-    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1], 
-    [0, 1, 0, 1, 1, 1, 1, 0, 0, 0], 
-    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0], 
-    [0, 1, 1, 1, 1, 1, 1, 0, 1, 0], 
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]], 1))
+        return -1 if min_moves == max_value else min_moves
