b8542d8d9f8f49eadfaca8280c6475c089857134
Harsh Shah <harshshah1295@gmail.com>
2022-10-21 13:11:34 -0300

    75 days learning plan

--- 75-study-plan/level-2/230-kth-smallest-element-in-a-bst.py
+++ 75-study-plan/level-2/230-kth-smallest-element-in-a-bst.py
@@ -8,4 +8,22 @@ class TreeNode:
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
+
+        def get_k_min(node, k, result):
+            if result[1] == k or node == None:
+                return
+
+            get_k_min(node.left, k, result)
+            if result[1] == k:
+                return
+            
+            result[0] = node.val
+            result[1] += 1
+            if result[1] == k:
+                return
+
+            get_k_min(node.right, k, result)
+
+        result = [0, 0]
+        get_k_min(root, k, result)
+        return result[0]
